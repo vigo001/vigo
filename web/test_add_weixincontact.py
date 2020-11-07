3ee7ac7ea4ce27b4b5b6b1cfe0eee66eb2c5f16c
1229348283@qq,com <vigo@1989>
2020-11-07 23:42:07 +0800

    使用cookie 登录企业微信，完成导入联系人，加上断言验证

--- web/test_add_weixincontact.py
+++ web/test_add_weixincontact.py
@@ -12,6 +12,8 @@ class TestTestdemo():
         options = Options()
         options.debugger_address = '127.0.1:9222'
         self.driver = webdriver.Chrome()
+        self.driver.maximize_window()
+        self.driver.implicitly_wait(4)
 
     def teardown_method(self):
         self.driver.quit()
@@ -76,51 +78,24 @@ class TestTestdemo():
             time.sleep(4)
 
     def test_shelve(self):
-        cookies = [
-            {'domain': '.work.weixin.qq.com', 'httpOnly': False, 'name': 'wwrtx.vid', 'path': '/', 'secure': False,
-             'value': '1688850280623055'},
-            {'domain': '.work.weixin.qq.com', 'httpOnly': True, 'name': 'wwrtx.vst', 'path': '/', 'secure': False,
-             'value': '9ty12OfXwe-tlvSL6f_xUOYXkNANzElWuSLK7MJprx8exI4lBVrYplNfkW2z67wDB7Nu5RO9udLcgKEiFgJcTxY6IYi1G7mT2hQS_2WN1XTWok9E7pPlWxf-PYgzwxudh72yq5ywlcFfKj1LSKaUDyFyLPYmfyIUUa961E84TXh_Wa8jfwWItwdL9uGnWsyq2BeEkrihrXpdx2LeRwz3fQOfctXytpix3MTQen6UMgIki7R_qB0tCu39S7yNp4610tOrvE6708uB7NDvrmQ8jw'},
-            {'domain': '.work.weixin.qq.com', 'httpOnly': False, 'name': 'wxpay.vid', 'path': '/', 'secure': False,
-             'value': '1688850280623055'},
-            {'domain': '.work.weixin.qq.com', 'httpOnly': False, 'name': 'wxpay.corpid', 'path': '/', 'secure': False,
-             'value': '1970325065179391'},
-            {'domain': '.work.weixin.qq.com', 'httpOnly': False, 'name': 'wwrtx.d2st', 'path': '/', 'secure': False,
-             'value': 'a5048037'},
-            {'domain': '.work.weixin.qq.com', 'httpOnly': True, 'name': 'wwrtx.sid', 'path': '/', 'secure': False,
-             'value': 'VyAWKCoDKOzjzq95NQ8s2058NCn-sQoAc0fbxrkop64yLKQE8rYj64tltMAqK8H6'},
-            {'domain': '.qq.com', 'httpOnly': False, 'name': 'pgv_info', 'path': '/', 'secure': False,
-             'value': 'ssid=s7613154616'},
-            {'domain': '.work.weixin.qq.com', 'httpOnly': True, 'name': 'wwrtx.refid', 'path': '/', 'secure': False,
-             'value': '3469797473431313'},
-            {'domain': 'work.weixin.qq.com', 'expiry': 1604778971, 'httpOnly': True, 'name': 'ww_rtkey', 'path': '/',
-             'secure': False, 'value': 'eajrql'},
-            {'domain': '.qq.com', 'expiry': 1604845228, 'httpOnly': False, 'name': '_gid', 'path': '/', 'secure': False,
-             'value': 'GA1.2.162069707.1604747436'},
-            {'domain': '.qq.com', 'expiry': 1667830828, 'httpOnly': False, 'name': '_ga', 'path': '/', 'secure': False,
-             'value': 'GA1.2.723290983.1604747436'},
-            {'domain': '.work.weixin.qq.com', 'expiry': 1636283435, 'httpOnly': False, 'name': 'wwrtx.c_gdpr',
-             'path': '/', 'secure': False, 'value': '0'},
-            {'domain': '.qq.com', 'expiry': 1606713036, 'httpOnly': False, 'name': 'ptui_loginuin', 'path': '/',
-             'secure': False, 'value': '1229348283'},
-            {'domain': '.work.weixin.qq.com', 'httpOnly': True, 'name': 'wwrtx.ltype', 'path': '/', 'secure': False,
-             'value': '1'},
-            {'domain': '.work.weixin.qq.com', 'httpOnly': True, 'name': 'wwrtx.ref', 'path': '/', 'secure': False,
-             'value': 'direct'},
-            {'domain': '.qq.com', 'expiry': 2147385600, 'httpOnly': False, 'name': 'pgv_pvid', 'path': '/',
-             'secure': False, 'value': '7967958306'},
-            {'domain': '.work.weixin.qq.com', 'expiry': 1636283928, 'httpOnly': False,
-             'name': 'Hm_lvt_9364e629af24cb52acc78b43e8c9f77d', 'path': '/', 'secure': False, 'value': '1604747704'},
-            {'domain': '.work.weixin.qq.com', 'expiry': 1607350830, 'httpOnly': False, 'name': 'wwrtx.i18n_lan',
-             'path': '/', 'secure': False, 'value': 'zh'},
-            {'domain': '.qq.com', 'expiry': 2147483797, 'httpOnly': False, 'name': 'ptcz', 'path': '/', 'secure': False,
-             'value': 'd764da7fa9650b8f27d8741e2536596aa93c6daf52c1ed63ff5d3af67a9996f9'},
-            {'domain': '.qq.com', 'expiry': 2147483737, 'httpOnly': False, 'name': 'RK', 'path': '/', 'secure': False,
-             'value': 'nm4sGbEyal'},
-            {'domain': '.qq.com', 'expiry': 1604758888, 'httpOnly': False, 'name': '_gat', 'path': '/', 'secure': False,
-             'value': '1'},
-            {'domain': '.qq.com', 'expiry': 2147385600, 'httpOnly': False, 'name': 'pgv_pvi', 'path': '/',
-             'secure': False, 'value': '68361216'}]
-    db = shelve.open("cookies")
-    db['cookie'] = cookies
-    db.close()
+
+        db = shelve.open("cookies")
+        cookies = db['cookie']
+
+        self.driver.get("https://work.weixin.qq.com/wework_admin/frame#index")
+        for cookie in cookies:
+            self.driver.add_cookie(cookie)
+        self.driver.refresh()
+        time.sleep(3)
+        # 点击导入通讯录
+        self.driver.find_element(By.CSS_SELECTOR, ".index_service_cnt_itemWrap:nth-child(2)").click()
+        # 上传
+        self.driver.find_element(By.CSS_SELECTOR, ".ww_fileImporter_fileContainer_uploadInputMask").send_keys(
+            'D:\\test20201107.xlsx')
+        # 验证
+        filename = self.driver.find_element(By.CSS_SELECTOR, ".ww_fileImporter_fileContainer_fileNames").text
+        assert "test20201107.xlsx" == filename
+
+
+
+
